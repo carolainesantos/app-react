98aeb6d21729ab565e61dff0fed37861a95cc7bd
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-03-15 16:03:36 -0300

    Iniciando app

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicial</Link>
+          </li>
+          <li>
+            <Link to="/sobre">Sobre</Link>
+          </li>
+          <li>
+            <Link to="/contato">Contato</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
